053e021fd3e09c5a2c1580bf7883fba765a1d164
安河内 哲朗 <yasukochi@atrasc.co.jp>
2020-02-04 14:34:36 +0900

    aa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req, res) => res.send("Hello, ATRASC!"));
+app.get('/',(req, res) => res.send(" Hello, ATRASC! "));
 app.listen(5000, () => console.log("connected!"));
