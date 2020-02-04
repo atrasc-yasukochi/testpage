e3fd4ee13d253f9f1febfced1cde8fad63b877a1
安河内 哲朗 <yasukochi@atrasc.co.jp>
2020-02-04 14:33:31 +0900

    debug commit

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => res.send("Hello, ATRASC!"));
-app.listen(5000, () => console.log("connected!"));
+app.listen(5000, () => console.log(" connected! "));
